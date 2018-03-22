36dae9a93a2a560b589f075f4f78ba9c3cdaaf3b
rozam <rozam@172.19.127.3>
2018-03-22 12:16:46 +0200

    first commit

---
+++ src/testing/test.java
@@ -0,0 +1,25 @@
+package testing;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class test {
+static WebDriver driver;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.setProperty("webdriver.gecko.driver", "C:\\gecko\\geckodriver.exe");
+driver=new FirefoxDriver();
+driver.get("https://ar-ar.facebook.com/");
+driver.manage().window().maximize();
+driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("raya.mah.987@gmail.com");
+driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("rayamah987");
+
+Select x=new Select(driver.findElement(By.xpath(".//*[@id='month']")));
+x.selectByIndex(6);
+driver.findElement(By.xpath(".//*[@id='loginbutton']")).click();
+	}
+
+}
---
+++ src/testing/test1.java
@@ -0,0 +1,48 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import static org.junit.Assert.*;
+class test1 {
+	static WebDriver driver;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "C:\\gecko\\geckodriver.exe");
+		driver=new FirefoxDriver();
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		driver.get("https://ar-ar.facebook.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("raya.mah.987@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("rayamah987");
+		Select x=new Select(driver.findElement(By.xpath(".//*[@id='month']")));
+		x.selectByIndex(6);
+		driver.findElement(By.xpath(".//*[@id='loginbutton']")).click();
+	}
+
+}
